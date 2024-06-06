40ab297c88a465a180455fd046c63ba5520d6f85
Wladislaw Krinitsin <krinitsin@iff1544.iff.kfa-juelich.de>
2024-06-06 17:20:31 +0200

    some more dmrg runs

--- scripts/spinN_mps/sbatch_dmrg2.sh
+++ scripts/spinN_mps/sbatch_dmrg2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH   --job-name=dmrg1004
-#SBATCH --output=log_dmrg1004%a
-#SBATCH  --error=log_dmrg1004%a
+#SBATCH   --job-name=dmrg1006
+#SBATCH --output=log_dmrg1006%a
+#SBATCH  --error=log_dmrg1006%a
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=48
 ##SBATCH --cores-per-socket=24
-#SBATCH --array=1-11
+#SBATCH --array=1-7
 
 # julia -t 48 dmrg2.jl 100 ${SLURM_ARRAY_TASK_ID} 1
-julia -t 48 dmrg2.jl 100 4 ${SLURM_ARRAY_TASK_ID}
+julia -t 48 dmrg2.jl 100 6 ${SLURM_ARRAY_TASK_ID}
